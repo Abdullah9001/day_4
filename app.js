4ce3b602e58e91cc3642407e482c635e60845ab1
Abdullah9001 <abdullahjubayer44@gmail.com>
2023-05-22 19:43:01 +0600

    if else lession done

---
+++ app.js
@@ -0,0 +1,59 @@
+let x = 6;
+let y = 3;
+console.log(x == 6);
+console.log(x == "6");
+console.log(x != 6);
+console.log(x != "6");
+console.log(x != 7);
+console.log(x != "7");
+console.log(x === 6);
+console.log(x === "6");
+console.log(x !== 6);
+console.log(x !== "6");
+
+console.log(x > 7);
+console.log(x < 7);
+console.log(x <= 7);
+console.log(x >= 7);
+console.log(x >= 6);
+console.log(x <= 6);
+
+let age = 18;
+if (age >= 18) {
+  console.log("You are adult!");
+} else {
+  console.log("You are so young");
+}
+
+if (x < 10 && y > 1) {
+  console.log("This is true");
+} else {
+  ("this is not true");
+}
+
+if (age == 16 || age == 17 || age == 18) {
+  console.log("This is ok");
+} else {
+  console.log("this is not ok");
+}
+
+console.log(age >= 18 ? "you ar young enough" : "you ar a little baby");
+
+let title = "Sakib";
+title = Number(title);
+
+if (isNaN(title)) {
+  console.log("Title is not a number");
+} else {
+  console.log(age == "Sakib" ? "I am Sakib" : "I dont know you");
+}
+
+let age2 = 38;
+
+if (age2 < 18) {
+  console.log("you are so young");
+} else if (age2 == 18) {
+  console.log("Welcome adult");
+} else {
+  console.log("You are old");
+}
